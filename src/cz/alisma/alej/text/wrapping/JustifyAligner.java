1dca9f373a2c674f1f44b2e50e229f1072de4d45
kubajj <vilouskubajj@gmail.com>
2018-05-04 13:43:37 +0200

    Test

--- src/cz/alisma/alej/text/wrapping/JustifyAligner.java
+++ src/cz/alisma/alej/text/wrapping/JustifyAligner.java
@@ -3,7 +3,6 @@ package cz.alisma.alej.text.wrapping;
 import java.util.List;
 
 public class JustifyAligner implements Aligner {
-
 	@Override
 	public String format(List<String> words, int widthofline, int width) {		
 		StringBuilder result = new StringBuilder();	
