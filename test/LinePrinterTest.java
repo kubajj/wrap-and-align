1258dd838a381758b260b48cb9c6f1f70791ae70
kubajj <vilouskubajj@gmail.com>
2018-05-25 00:12:56 +0200

    Ts

--- test/LinePrinterTest.java
+++ test/LinePrinterTest.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package cz.alisma.alej.text.wrapping;
+
 
 import org.junit.After;
 import org.junit.Before;
